8906fcd34a8e7f111970d1b18969535e743f2109
linyan <1439713475@qq.com>
2022-11-14 12:07:51 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+const container = ReactDom.createRoot(
+	document.getElementById("root") as HTMLElement,
+);
+
+container.render(<App />);
