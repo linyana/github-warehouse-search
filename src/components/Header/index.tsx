00351c8b9700cd0b573b39202e43f0047b81c7a3
linyan <1439713475@qq.com>
2022-11-16 15:16:11 +0800

    增加了登录页面

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { changeTheme } from "../../store/theme";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../../store";
+import { useNavigate } from "react-router";
 
 import MyNavLink from "../MyNavLink";
 
@@ -12,13 +13,39 @@ const App: React.FC = () => {
   const handleChangeTheme = () => {
     dispatch(changeTheme());
   };
+  const theme = useSelector((state: RootState) => state.theme.theme);
+
+  const navigate = useNavigate();
+
+  const themeSrc =
+    theme === "dark"
+      ? require("../../assets/images/dark.png")
+      : require("../../assets/images/light.png");
+
+  const loginSrc =
+    theme === "dark"
+      ? require("../../assets/images/logindark.png")
+      : require("../../assets/images/loginlight.png");
+
   return (
     <div className="header">
       <div className="navs">
         <MyNavLink to="home">首页</MyNavLink>
         <MyNavLink to="list">列表</MyNavLink>
       </div>
-      <button onClick={handleChangeTheme}>改变主题</button>
+      <div className="nav_right">
+        <div className="change_theme" onClick={handleChangeTheme}>
+          <img src={themeSrc} alt="" />
+        </div>
+        <div
+          className="login"
+          onClick={() => {
+            navigate("/login", { replace: false });
+          }}
+        >
+          <img src={loginSrc} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import { RouteObject, Navigate } from "react-router-dom";
 // 一级路由
 const Layout = lazy(() => import("../pages/Layout"));
 const NotFound = lazy(() => import("../pages/NotFound"));
+const Login = lazy(() => import("../pages/Login"));
 
 // 二级路由
 const Home = lazy(() => import("../pages/Home"));
@@ -11,7 +12,7 @@ const List = lazy(() => import("../pages/List"));
 
 const routes: RouteObject[] = [
   {
-    path: "/",
+    path: "/user",
     element: <Layout />,
     children: [
       {
@@ -27,6 +28,14 @@ const routes: RouteObject[] = [
   {
     path: "/404",
     element: <NotFound />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Navigate to="/user/home" />,
   },
   {
     path: "/*",
