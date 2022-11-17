859d7081931a9f3e0483219425c743bd0c1a258c
linyan <1439713475@qq.com>
2022-11-17 14:00:27 +0800

    优化了404页面

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "../../components/Header";
+
+import "./index.css";
 
 export default function App() {
-  return <div>404</div>;
+  return (
+    <>
+      <Header></Header>
+      <div className="notfound">
+        <div className="notfound_title">404</div>
+        <div className="notfound_text">页面找不到了</div>
+      </div>
+    </>
+  );
 }
