0474ade577d1efe7c22a8fd54e7039e893c97189
linyan <1439713475@qq.com>
2022-11-18 10:46:27 +0800

    改为hash路由

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux/es/exports";
 import { store } from "./store";
 
@@ -11,8 +11,8 @@ const container = ReactDom.createRoot(
 
 container.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
